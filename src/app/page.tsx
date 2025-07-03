63f90d315520916adb8b2f4f5e04585242916a67
Riad <riadk910@gmail.com>
2025-07-03 16:48:36 +0600

    Testing dark mode toggle through page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { ModeToggle } from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
 import {
   SignedIn,
   SignedOut,
@@ -11,7 +13,9 @@ const homePage = () => {
   return (
     <div>
       <SignedOut>
-        <SignInButton mode="modal" />
+        <SignInButton mode="modal">
+          <Button variant={"outline"}>Sign In</Button>
+        </SignInButton>
         <SignUpButton mode="modal">
           <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
             Sign Up
@@ -21,6 +25,8 @@ const homePage = () => {
       <SignedIn>
         <UserButton />
       </SignedIn>
+
+      <ModeToggle />
     </div>
   );
 };
