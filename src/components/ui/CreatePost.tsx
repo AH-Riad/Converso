efa43c0b23be75d80ae7fdeec3a4615536938784
Riad <riadk910@gmail.com>
2025-07-08 13:53:55 +0600

    Added photo button with functionality

--- src/components/ui/CreatePost.tsx
+++ src/components/ui/CreatePost.tsx
@@ -5,34 +5,52 @@ import { useState } from "react";
 import { Card, CardContent } from "./card";
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 import { Textarea } from "./textarea";
+import { Button } from "./button";
+import { Ghost, ImageIcon } from "lucide-react";
 
 function CreatePost() {
   const { user } = useUser(); //client component cant be async that why we are using useUSer()
   const [content, setContent] = useState<string>("");
-  const [imagrUrl, setImageUrl] = useState<string>("");
+  const [imageUrl, setImageUrl] = useState<string>("");
+  const [showImageUpload, setShowImageUpload] = useState<boolean>(false);
   const [isPosting, setIsPosting] = useState<boolean>(false);
   return (
-    <div>
-      <Card className="mb-6">
-        <CardContent className="pt-6">
-          <div className="space-y-4">
-            <div className="flex space-x-4">
-              <Avatar>
-                <AvatarImage src={user?.imageUrl || "/avatar.png"} />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-              <Textarea
-                placeholder="What's on your mind?"
-                className="min-h[100px] resize-none border-none focus-visible:ring-0 p-0 text-base"
-                value={content}
-                onChange={(e) => setContent(e.target.value)}
+    <Card className="mb-6">
+      <CardContent className="pt-6">
+        <div className="space-y-4">
+          <div className="flex space-x-4">
+            <Avatar>
+              <AvatarImage src={user?.imageUrl || "/avatar.png"} />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <Textarea
+              placeholder="What's on your mind?"
+              className="min-h[100px] resize-none border-none focus-visible:ring-0 p-0 text-base"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              disabled={isPosting}
+            />
+          </div>
+          {/* TODO: HANDLE IMAGE UPLOADS */}
+
+          <div className="flex items-center justify-between border-t pt-4">
+            <div className="flex space-x-2">
+              <Button
+                type="button"
+                variant={"ghost"}
+                size="sm"
+                className="text-muted-foreground  hover:text-primary"
+                onClick={() => setShowImageUpload(!showImageUpload)}
                 disabled={isPosting}
-              />
+              >
+                <ImageIcon className="size-4 mr-2" />
+                Photo
+              </Button>
             </div>
           </div>
-        </CardContent>
-      </Card>
-    </div>
+        </div>
+      </CardContent>
+    </Card>
   );
 }
 
