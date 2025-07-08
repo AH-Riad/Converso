45c34ac764bf6c32c094de9c94c665836298d745
Riad <riadk910@gmail.com>
2025-07-08 12:42:45 +0600

    Avatar implemented

--- src/components/ui/CreatePost.tsx
+++ src/components/ui/CreatePost.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { useState } from "react";
+import { Card, CardContent } from "./card";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+import { Textarea } from "./textarea";
 
 function CreatePost() {
-  return <div>CreatePost</div>;
+  const { user } = useUser(); //client component cant be async that why we are using useUSer()
+  const [content, setContent] = useState<string>("");
+  const [imagrUrl, setImageUrl] = useState<string>("");
+  const [isPosting, setIsPosting] = useState<boolean>(false);
+  return (
+    <div>
+      <Card className="mb-6">
+        <CardContent className="pt-6">
+          <div className="space-y-4">
+            <div className="flex space-x-4">
+              <Avatar>
+                <AvatarImage src={user?.imageUrl || "/avatar.png"} />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
 
 export default CreatePost;
