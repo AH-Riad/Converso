ea732f512c5cb95508ebd43593fbbe2a61664f2c
Riad <riadk910@gmail.com>
2025-07-11 17:40:05 +0600

    FollowButton Initialized

---
+++ src/components/ui/FollowButton.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "./button";
+import { LoaderIcon } from "react-hot-toast";
+
+function FollowButton({ userId }: { userId: number }) {
+  const [isLoading, setIsLoading] = useState();
+
+  const handleFollow = async () => {};
+  return (
+    <Button
+      size={"sm"}
+      variant={"secondary"}
+      onClick={handleFollow}
+      disabled={isLoading}
+      className="w-20"
+    >
+      {isLoading ? <LoaderIcon /> : "Follow"}
+    </Button>
+  );
+}
+
+export default FollowButton;
