0dfbfa06bbd2d045be1a130172d10f9c16ab7a66
Riad <riadk910@gmail.com>
2025-07-13 13:37:59 +0600

    Updated WhoToFOllow.tsx

--- src/components/ui/WhoToFollow.tsx
+++ src/components/ui/WhoToFollow.tsx
@@ -8,43 +8,43 @@ import FollowButton from "./FollowButton";
 async function WhoToFollow() {
   const users = await getRandomUser();
   if (users.length === 0) return null;
+
   return (
-    <Card>
+    <Card className="shadow-md border rounded-xl">
       <CardHeader>
         <CardTitle>People you may follow</CardTitle>
       </CardHeader>
-      <CardContent>
-        <div className="space-y-4">
-          {users.map((user) => (
-            <div
-              key={user.id}
-              className="flex gap-2 items-center justify-between"
-            >
-              <div className="flex items-center gap-1">
-                <Link href={`/profile/${user.username}`}>
-                  <Avatar>
-                    <AvatarImage
-                      src={user.image ?? "/avatar.png"}
-                    ></AvatarImage>
-                  </Avatar>
+      <CardContent className="max-h-[70vh] overflow-y-auto pr-2 space-y-4">
+        {users.map((user) => (
+          <div
+            key={user.id}
+            className="flex gap-2 items-center justify-between"
+          >
+            <div className="flex items-center gap-2">
+              <Link href={`/profile/${user.username}`}>
+                <Avatar>
+                  <AvatarImage
+                    src={user.image ?? "/avatar.png"}
+                    alt={user.name ?? undefined}
+                  />
+                </Avatar>
+              </Link>
+              <div className="text-sm">
+                <Link
+                  href={`/profile/${user.username}`}
+                  className="font-medium cursor-pointer"
+                >
+                  {user.name}
                 </Link>
-                <div className="text-sm">
-                  <Link
-                    href={`/profile/${user.username}`}
-                    className="font-medium cursor-pointer"
-                  >
-                    {user.name}
-                  </Link>
-                  <p className="text-muted-foreground">@{user.username}</p>
-                  <p className="text-muted-foreground">
-                    {user._count.followers} followers
-                  </p>
-                </div>
+                <p className="text-muted-foreground">@{user.username}</p>
+                <p className="text-muted-foreground">
+                  {user._count.followers} followers
+                </p>
               </div>
-              <FollowButton userId={user.id} />
             </div>
-          ))}
-        </div>
+            <FollowButton userId={user.id} />
+          </div>
+        ))}
       </CardContent>
     </Card>
   );
