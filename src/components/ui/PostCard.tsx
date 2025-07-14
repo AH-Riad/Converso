21c264e14fc564796cb6ed4aa9075dd36ded2161
Riad <riadk910@gmail.com>
2025-07-14 12:35:26 +0600

    Added handleLike function

--- src/components/ui/PostCard.tsx
+++ src/components/ui/PostCard.tsx
@@ -12,8 +12,19 @@ function PostCard({ post, dbUserId }: { post: Post; dbUserId: string | null }) {
   const [isCommenting, setIsCommenting] = useState<boolean>(false);
   const [isLiking, setIsLiking] = useState<boolean>(false);
   const [isDeleting, setIsDeleting] = useState<boolean>(false);
-  const [hasLiked, setHadLiked] = useState<boolean>(false);
+  const [hasLiked, setHasLiked] = useState<boolean>(false);
   const [optimisticLikes, setOptimisticLikes] = useState(post._count.likes);
+
+  const handleLike = async () => {
+    if (isLiking) return;
+    try {
+      setIsLiking(true);
+      setHasLiked((prev) => !prev);
+      setOptimisticLikes((prev) => prev(hasLiked ? -1 : 1));
+    } catch (error) {
+    } finally {
+    }
+  };
 
   return <div>PostCard</div>;
 }
