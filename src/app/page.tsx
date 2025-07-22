07b952131057c9d67557b2008ce3bf41b3f935ca
Riad <riadk910@gmail.com>
2025-07-22 10:15:33 +0600

    Date-fns installed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default async function Page() {
     <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 px-4">
       {/* Main Feed */}
       <div className="lg:col-span-8 space-y-6">
-        {user && <CreatePost />}
+        {user ? <CreatePost /> : null}
         {posts.map((post) => (
           <PostCard key={post.id} post={post} dbUserId={dbUserId} />
         ))}
