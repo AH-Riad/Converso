566efa1f364ec93cee18720f6a2749d9682e9f46
Riad <riadk910@gmail.com>
2025-08-01 17:04:40 +0600

    Handled profile section and fixed some issues

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,12 +1,50 @@
-"use client";
+import {
+  getProfileByUsername,
+  getUserLikedPosts,
+  getUserPosts,
+  isFollowing,
+} from "@/actions/profile";
+import { notFound } from "next/navigation";
+import ProfilePageClient from "./ProfilePageClient";
 
-import { useParams } from "next/navigation";
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ username: string }>;
+}) {
+  const resolvedParams = await params;
+  const user = await getProfileByUsername(resolvedParams.username);
+  if (!user) return;
 
-export default function Page() {
-  const params = useParams();
-  const username = params.username as string;
-
-  console.log("Username:", username);
-
-  return <p>Post: {username}</p>;
+  return {
+    title: `${user.name ?? user.username}`,
+    description: user.bio || `Check out ${user.username}'s profile.`,
+  };
 }
+
+async function ProfilePageServer({
+  params,
+}: {
+  params: Promise<{ username: string }>;
+}) {
+  const resolvedParams = await params;
+  const user = await getProfileByUsername(resolvedParams.username);
+
+  if (!user) notFound();
+
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id),
+  ]);
+
+  return (
+    <ProfilePageClient
+      user={user}
+      posts={posts}
+      likedPosts={likedPosts}
+      isFollowing={isCurrentUserFollowing}
+    />
+  );
+}
+export default ProfilePageServer;
